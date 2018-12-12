83c57aa1de591bde6ca7d7878d1efa4660d63515
JanosikOpryszek <ahimm44@gmail.com>
2018-12-12 14:35:55 +0000

    2

--- readsend.cpp
+++ readsend.cpp
@@ -17,38 +17,42 @@ main(void)
 	int s;
 	int nbytes;
 	char  buff[20];
-	struct sockaddr_can addr;
-	struct can_frame frame;
-	struct ifreq ifr;
-
 	const char *ifname = "can0";
 
+	//1 SOCKET CREATE  
 	if((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
 		perror("Error while opening socket");
 		return -1;
 	}
+	
+	struct sockaddr_can addr;
+	struct can_frame frame;
+	struct ifreq ifr;
 
 	strcpy(ifr.ifr_name, ifname);
 	ioctl(s, SIOCGIFINDEX, &ifr);
 	
+
+	//2 ACTIVATE STRUCTURE
 	addr.can_family  = AF_CAN;
 	addr.can_ifindex = ifr.ifr_ifindex;
 
 	printf("%s at index %d\n", ifname, ifr.ifr_ifindex);
 
+	//3 BINDING socket
 	if(bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("Error in socket bind");
 		return -2;
 	}
-while(1)
-{
+
 	frame.can_id  = 0x0ff;
 	frame.can_dlc = 2;
 	frame.data[0] = 0xff;
 	frame.data[1] = 0xff;
+
+while(1)
+{
 	int i=0;
-
-
 
 	nbytes = write(s, &frame, sizeof(struct can_frame));
 	printf("Wrote %d bytes\n", nbytes);
